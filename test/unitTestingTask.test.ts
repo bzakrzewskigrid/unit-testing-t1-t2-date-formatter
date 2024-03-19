62223a02c3eb7af2cb7854f6d49a1f045120cf8c
bzakrzewski <bzakrzewski@griddynamics.com>
2024-03-19 18:41:31 +0100

    reformat: Shorten arrays of objects in it.each

--- test/unitTestingTask.test.ts
+++ test/unitTestingTask.test.ts
@@ -14,128 +14,41 @@ describe('unitTestingTask test suite', () => {
   dateToString = date.toString();
 
   it.each([
-    {
-      format: 'YYYY',
-      expected: '2020',
-    },
-    {
-      format: 'YY',
-      expected: '20',
-    },
-    {
-      format: 'MMMM',
-      expected: 'January',
-    },
-    {
-      format: 'MMM',
-      expected: 'Jan',
-    },
-    {
-      format: 'MM',
-      expected: '01',
-    },
-    {
-      format: 'M',
-      expected: '1',
-    },
-    {
-      format: 'DDD',
-      expected: 'Monday',
-    },
-    {
-      format: 'DD',
-      expected: 'Mon',
-    },
-    {
-      format: 'D',
-      expected: 'Mo',
-    },
-    {
-      format: 'dd',
-      expected: '13',
-    },
-    {
-      format: 'd',
-      expected: '13',
-    },
-    {
-      format: 'HH',
-      expected: '15',
-    },
-    {
-      format: 'H',
-      expected: '15',
-    },
-    {
-      format: 'hh',
-      expected: '03',
-    },
-    {
-      format: 'h',
-      expected: '3',
-    },
-    {
-      format: 'mm',
-      expected: '05',
-    },
-    {
-      format: 'm',
-      expected: '5',
-    },
-    {
-      format: 'ss',
-      expected: '18',
-    },
-    {
-      format: 's',
-      expected: '18',
-    },
-    {
-      format: 'ff',
-      expected: '000',
-    },
-    {
-      format: 'f',
-      expected: '0',
-    },
-    {
-      format: 'A',
-      expected: 'PM',
-    },
-    {
-      format: 'a',
-      expected: 'pm',
-    },
-    {
-      format: 'ZZ',
-      expected: '+0000',
-    },
-    {
-      format: 'Z',
-      expected: '+00:00',
-    },
+    { format: 'YYYY', expected: '2020' },
+    { format: 'YY', expected: '20' },
+    { format: 'MMMM', expected: 'January' },
+    { format: 'MMM', expected: 'Jan' },
+    { format: 'MM', expected: '01' },
+    { format: 'M', expected: '1' },
+    { format: 'DDD', expected: 'Monday' },
+    { format: 'DD', expected: 'Mon' },
+    { format: 'D', expected: 'Mo' },
+    { format: 'dd', expected: '13' },
+    { format: 'd', expected: '13' },
+    { format: 'HH', expected: '15' },
+    { format: 'H', expected: '15' },
+    { format: 'hh', expected: '03' },
+    { format: 'h', expected: '3' },
+    { format: 'mm', expected: '05' },
+    { format: 'm', expected: '5' },
+    { format: 'ss', expected: '18' },
+    { format: 's', expected: '18' },
+    { format: 'ff', expected: '000' },
+    { format: 'f', expected: '0' },
+    { format: 'A', expected: 'PM' },
+    { format: 'a', expected: 'pm' },
+    { format: 'ZZ', expected: '+0000' },
+    { format: 'Z', expected: '+00:00' },
   ])(`'${dateToString}' formatted with allowed format: '$format' should be correct`, ({ format, expected }) => {
     const actual = sut(format, dateToString);
     expect(actual).toBe(expected);
   });
 
   it.each([
-    {
-      format: 'ISODate',
-      expected: '2020-01-13',
-    },
-    {
-      format: 'ISOTime',
-      expected: '03:05:18',
-    },
-    {
-      format: 'ISODateTime',
-      expected: '2020-01-13T03:05:18',
-    },
-    {
-      format: 'ISODateTimeTZ',
-      expected: '2020-01-13T03:05:18+00:00',
-    },
+    { format: 'ISODate', expected: '2020-01-13' },
+    { format: 'ISOTime', expected: '03:05:18' },
+    { format: 'ISODateTime', expected: '2020-01-13T03:05:18' },
+    { format: 'ISODateTimeTZ', expected: '2020-01-13T03:05:18+00:00' },
   ])(`'${dateToString}' formatted with basic format: '$format' should be correct`, ({ format, expected }) => {
     const actual = sut(format, dateToString);
     expect(actual).toBe(expected);
